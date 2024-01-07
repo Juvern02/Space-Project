8c7b59effe9eb3a4aa11a2127789207d9dcb9d8e
Mohammad Altaha <ec21420@qmul.ac.uk>
2024-01-07 16:59:13 +0000

    keeping data between scenes (has player inital position bug)

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -4,8 +4,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneManagerScript : MonoBehaviour
 {
+    private Transform player;
+
+    private void Awake() {
+        player = GameObject.Find("PlayerArmature").transform;
+
+        if (player == null)
+        {
+            Debug.Log("PlayerArmature not found in the scene");
+        }
+        
+    }
     public void LoadScene(string sceneName){
+        if (sceneName == "InsideSpaceShip")
+        {
+            SetPlayerPosition();
+        }
+
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void SetPlayerPosition()
+    {
+        if (player != null)
+        {
+            Vector3 newPosition = new(-44, 1, -50);
+            player.position = newPosition;
+            Debug.Log("New Position: " + newPosition);
+
+
+            
+        }
+        else{
+            Debug.LogError("player not assigned!");
+        }
+        
     }
 
 }
