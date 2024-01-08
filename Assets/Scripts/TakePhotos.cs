7870917f2776b815c33ee4922e609d0403788576
mariamg03 <98821978+mariamg03@users.noreply.github.com>
2024-01-08 18:00:16 +0000

    screenshot fixed

--- Assets/Scripts/TakePhotos.cs
+++ Assets/Scripts/TakePhotos.cs
@@ -6,12 +6,27 @@ using System;
 
 public class TakePhoto : MonoBehaviour
 {
+    public GameObject PhotoCaptureCanvas;
+    public float displayTime = 0.3f;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))
         {
             ScreenCapture.CaptureScreenshot("screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".png", 4);
+            Invoke("TextDisplay", displayTime);
         }
+    }
+
+    void TextDisplay()
+    {
+        PhotoCaptureCanvas.SetActive(true);
+        Invoke("DisableCanvas", displayTime);
+    }
+
+    void DisableCanvas()
+    {
+        PhotoCaptureCanvas.SetActive(false);
     }
 }
