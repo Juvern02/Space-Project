46da99a36af0005f1060ab80a462550f44b2ff7e
abi <a.ravindran@se21.qmul.ac.uk>
2024-01-08 18:43:46 +0000

    ...

--- Assets/Scripts/audiomanager.cs
+++ Assets/Scripts/audiomanager.cs
@@ -11,10 +11,16 @@ public class audiomanager : MonoBehaviour
     [Header("----Audio Clip----")]
     public AudioClip background;
     public AudioClip wind;
+    public AudioClip steps;
+    
 
     private void Start()
     {
         musicSource.clip =background;
         musicSource.Play();
+    }
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
     }
 }
